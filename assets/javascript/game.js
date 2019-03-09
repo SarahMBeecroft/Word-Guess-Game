48b229e75b53c2649b66287756bbbef793f0b81f
Sarah B <smblair87@gmail.com>
2019-03-09 07:52:46 -0800

    Linked to JS on index.html, started writing pseudo-code, and added js to pick random word from array

---
+++ assets/javascript/game.js
@@ -0,0 +1,23 @@
+// Star Wars words for guesses
+var words = ["tatooine", "poe dameron", "lando calrissian", "millennium falcon", "sheev palpatine", "battle droid", "cross-saber", "rebel alliance", "endor", "tie interceptor"];
+
+// Generates random number between 0 and length of array
+var number = Math.floor(Math.random() * words.length);
+
+// Make sure words.length pulls up right number
+console.log(words.length)
+
+// Pick random word from var words 
+var word = words[number];
+
+// Assign random word to underscores for games 
+
+// Set up ontouch for user to guess
+
+// If letter is in word, add to underscore. Else, mark as 1 guess
+
+// Update number of guess remaining
+
+
+
+

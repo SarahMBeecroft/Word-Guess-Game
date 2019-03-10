28c12791b2fe685ec90149a6b462b155fce1b9d6
Sarah B <smblair87@gmail.com>
2019-03-10 15:42:51 -0700

    Renamed variables and organized at top of game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,56 +1,54 @@
-// All variables
+// Global variables
+
 // Array of Star Wars words
-var words = ["tatooine", "poe dameron", "lando calrissian", "millennium falcon", "jyn erso", "battle droid", "cross saber", "rebel alliance", "endor", "tie interceptor"];
+var words = ["tatooine", "chewbacca", "lando", "ewoks", "empire", "hoth", "crosssaber", "rebels", "endor", "obiwan"];
+// Choose word randomly function
+var getRandomWord = Math.floor(Math.random() * words.length);
+// Picks word from random word function
+var chosenWord = words[getRandomWord];
 // Array for right guesses
 var rightGuesses = [];
 // Array for wrong guesses
 var wrongGuesses = [];
 // Array for underscores
-var underscores = [];
+var underScores = [];
 
-
-
-// Generates random word between 0 and length of array
-var randomWord = words[Math.floor(Math.random() * words.length)];
-
-// Tests random word
-console.log(randomWord);
 
 // Make sure words.length pulls up right number (10)
 console.log(words.length)
-
-// Picks random word from words array  
-var getRandomWord = function () {
-    return words[Math.floor(Math.random() * words.length)];
-};
 
 // Function that creates underscores based on length of random word
 function generateUnderscore () {
     // For Loop
     var i;
-    for (i = 0; i < randomWord.length; i++) { 
-    underscores.push("_");
+    for (i = 0; i < getRandomWord.length; i++) { 
+    underScores.push("_");
     }
-    return underscores;
+    return underScores;
 }
-
-// Test generateUnderscore function
-console.log(generateUnderscore());
 
 // Function to set up ontouch for user to guess words
 document.onkeyup = function(event) {
     // Gets users key stroke
-    var keyword = String.fromCharCode(event.keyCode);
+    var keyword = String.fromCharCode(event.gitkeyCode);
     // Compare key code to randomly chosen word
-    if(randomWord.indexOf(keyword) > -1) {
+    if(getRandomWord.indexOf(keyword) > -1) {
         // Pushes to right guesses array
         rightGuesses.push(keyword);
-        console.log(rightGuesses); 
+        // Replaces underscore with right letter
+        underScores[getRandomWord.indexOf(keyword)] = keyword;
+        // Checks to see if user word matches random word
+        if(underscores.join("") == getRandomWord) {
+            console.log(underscores);
+            // alert for now
+            alert("You Win!");
+        }
     }
 
     else {
         // Pushes to wrong guesses array
         wrongGuesses.push(keyword);
+        // testing 
         console.log(wrongGuesses);
     }
      
@@ -58,7 +56,6 @@ document.onkeyup = function(event) {
 
 // Prints underscores to HTML
 document.getElementsByClassName("underscores").innerHTML = "event";
-
 
 // If letter is in word, add to underscore. Else, mark as 1 guess
 

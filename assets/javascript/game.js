b241a789073aea33d9ee62532d2c9af781c96246
Sarah B <smblair87@gmail.com>
2019-03-09 17:03:10 -0800

    Created variable for underscores and function to create underscores based on length of random word

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,23 +1,37 @@
-// Star Wars words for guesses
-var words = ["tatooine", "poe dameron", "lando calrissian", "millennium falcon", "sheev palpatine", "battle droid", "cross-saber", "rebel alliance", "endor", "tie interceptor"];
+// Array of Star Wars words
+var words = ["tatooine", "poe dameron", "lando calrissian", "millennium falcon", "sheev palpatine", "battle droid", "cross saber", "rebel alliance", "endor", "tie interceptor"];
 
-// Generates random number between 0 and length of array
-var number = Math.floor(Math.random() * words.length);
+// Generates random word between 0 and length of array
+var randomWord = words[Math.floor(Math.random() * words.length)];
 
-// Make sure words.length pulls up right number
+// Tests random word
+console.log(randomWord);
+
+// Make sure words.length pulls up right number (10)
 console.log(words.length)
 
-// Pick random word from var words 
-var word = words[number];
+// Picks random word from words array  
+var getRandomWord = function () {
+    return words[Math.floor(Math.random() * words.length)];
+};
 
-// Assign random word to underscores for games 
+// Create variables for underscores
+var underscores = [];
+
+// Function that creates underscores based on length of random word
+function generateUnderscore () {
+    // For Loop
+    var i;
+    for (i = 0; i < randomWord.length; i++) { 
+    underscores.push("_")
+}
+}
 
 // Set up ontouch for user to guess
 
 // If letter is in word, add to underscore. Else, mark as 1 guess
 
 // Update number of guess remaining
-
 
 
 

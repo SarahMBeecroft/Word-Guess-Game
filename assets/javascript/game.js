ba440c214d3db00447db8a84394fb3e746c17732
Sarah B <smblair87@gmail.com>
2019-03-10 18:27:30 -0700

    Added functions to reset game and check for letters, updated variable names, set letters to uppercase, and added code to update DOM with results

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,65 +1,115 @@
-// Global variables
+//Global variables
 
 // Array of Star Wars words
 var words = ["tatooine", "chewbacca", "lando", "ewoks", "empire", "hoth", "crosssaber", "rebels", "endor", "obiwan"];
-// Choose word randomly function
-var getRandomWord = Math.floor(Math.random() * words.length);
-// Picks word from random word function
-var chosenWord = words[getRandomWord];
-// Array for right guesses
-var rightGuesses = [];
-// Array for wrong guesses
-var wrongGuesses = [];
-// Array for underscores
-var underScores = [];
+// Sets max number of guesses
+const maxGuess = 10
+// Resets game
+var pauseGame = false
+// Letters user has guessed
+var guessedLetters = [];
+// Word user is guessing
+var guessingWord = [];
+// Compares user's guess to chosen word
+var wordToMatch
+// Shows number of user guesses
+var numGuess
+// Shows wins
+var wins = 0
 
+//============================================================================
 
-// Make sure words.length pulls up right number (10)
-console.log(words.length)
+// Game code
 
-// Function that creates underscores based on length of random word
-function generateUnderscore () {
-    // For Loop
-    var i;
-    for (i = 0; i < getRandomWord.length; i++) { 
-    underScores.push("_");
+// Function to reset game
+resetGame()
+
+// Function to set up onkeypress for user to guess letters
+document.onkeypress = function (event) {
+    // Make sure key pressed is alpha character
+    if (isAlpha(event.key) && !pauseGame) {
+        // Sets letters to uppercase for display
+        checkForLetter(event.key.toUpperCase())
     }
-    return underScores;
 }
 
-// Function to set up ontouch for user to guess words
-document.onkeyup = function(event) {
-    // Gets users key stroke
-    var keyword = String.fromCharCode(event.gitkeyCode);
-    // Compare key code to randomly chosen word
-    if(getRandomWord.indexOf(keyword) > -1) {
-        // Pushes to right guesses array
-        rightGuesses.push(keyword);
-        // Replaces underscore with right letter
-        underScores[getRandomWord.indexOf(keyword)] = keyword;
-        // Checks to see if user word matches random word
-        if(underscores.join("") == getRandomWord) {
-            console.log(underscores);
-            // alert for now
-            alert("You Win!");
+// Checks to see if letter guessed is in guessing word
+function checkForLetter(letter) {
+    var foundLetter = false
+
+// Searches current string for letter guessed
+    for (var i = 0, j = wordToMatch.length; i < j; i++) {
+        if (letter === wordToMatch[i]) {
+            guessingWord[i] = letter
+            foundLetter = true
+            // If guessing word matches random word
+            if (guessingWord.join("") === wordToMatch) {
+                // Add +1 to wins
+                wins++
+                pauseGame = true
+                updateDisplay()
+                // Resets game after 5 seconds
+                setTimeout(resetGame, 5000)
+            }
         }
     }
 
-    else {
-        // Pushes to wrong guesses array
-        wrongGuesses.push(keyword);
-        // testing 
-        console.log(wrongGuesses);
+    if (!foundLetter) {
+        // Checks to see if inccorrect guess is already on the list
+        if (!guessedLetters.includes(letter)) {
+            // Add incorrect letter to guessed letter list
+            guessedLetters.push(letter)
+            // Decrease the number of remaining guesses
+            numGuess--
+        }
+        if (numGuess === 0) {
+            // Display word before resetting game
+            guessingWord = wordToMatch.split()
+            pauseGame = true
+            setTimeout(resetGame, 5000)
+        }
     }
-     
-    };
 
-// Prints underscores to HTML
-document.getElementsByClassName("underscores").innerHTML = "event";
+    updateDisplay()
 
-// If letter is in word, add to underscore. Else, mark as 1 guess
+}
 
-// Update number of guess remaining
+// Makes sure letter pressed is a-z (lower case or caps)
+function isAlpha(ch) {
+    return /^[A-Z]$/i.test(ch);
+}
+
+// Resets game once user reaches max guesses
+function resetGame() {
+    numGuess = maxGuess
+    pauseGame = false
+
+// Get a new word
+wordToMatch = words[Math.floor(Math.random() * words.length)].toUpperCase()
 
 
+// Reset word arrays
+guessedLetters = []
+guessingWord = []
 
+// Reset the guessed word
+    for (var i = 0, j = wordToMatch.length; i < j; i++) {
+        // Put a space instead of an underscore between multi word "words"
+        if (wordToMatch[i] === " ") {
+            guessingWord.push(" ")
+        } else {
+            guessingWord.push("_")
+        }
+    }
+
+// Function to update the DOM
+    updateDisplay()
+}
+
+// Updates HTML to show wins, curent word, remaining guesses, and letters already guessed
+function updateDisplay() {
+    document.getElementById("totalWins").innerHTML = wins
+    document.getElementById("currentWord").innerHTML = guessingWord.join("")
+    document.getElementById("remainingGuesses").innerHTML= numGuess
+    document.getElementById("guessedLetters").innerHTML = guessedLetters.join(" ")
+}
